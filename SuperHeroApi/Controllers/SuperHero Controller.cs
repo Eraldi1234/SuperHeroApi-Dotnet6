eecb2970705a343bcc3c70da60c854e4a80f90b2
User <User@ALDI>
2023-11-02 02:51:13 -0800

    Add project files.

---
+++ SuperHeroApi/Controllers/SuperHero Controller.cs
@@ -0,0 +1,78 @@
+﻿using DocumentFormat.OpenXml.InkML;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHero_Controller : ControllerBase
+    {
+        
+  
+        private readonly DataContext _context;
+
+        public SuperHero_Controller(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+
+        {
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+                return BadRequest("Stevi not found.");
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+        
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var dbhero = await _context.SuperHeroes.FindAsync(request.Id);
+            if (dbhero == null)
+                return BadRequest("Stevi not found.");
+            dbhero.Name = request.Name;
+            dbhero.FirstName = request.FirstName;
+            dbhero.LastName = request.LastName;
+            dbhero.Place = request.Place;
+            
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Detete(int id)
+        {
+            var dbhero = await _context.SuperHeroes.FindAsync(id);
+            if (dbhero == null)
+                return BadRequest("Stevi not found.");
+            _context.SuperHeroes.Remove(dbhero); 
+            await _context.SaveChangesAsync();  
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+
+
+
+
+
+
+    }
+    }
